cc8276c31d20d708cb5c20267de6ac06e8c51168
Maksym <efremovmaxim95@gmail.com>
2023-06-14 11:32:27 +0300

    Added Howl library, created player store for playing tracks

---
+++ src/includes/helper.js
@@ -0,0 +1,8 @@
+export default {
+  formatTime(time) {
+    let minutes = Math.floor(time / 60) || 0
+    let seconds = Math.round(time - minutes * 60) || 0
+
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`
+  }
+}
